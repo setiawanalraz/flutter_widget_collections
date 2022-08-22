074615f84c202ff5e51419cebc42bda89152157c
setiawanalraz <alrazsetiawan79@gmail.com>
2022-08-22 11:59:10 +0700

    Adding URL launcher package

--- lib/data/data_store.dart
+++ lib/data/data_store.dart
@@ -16,25 +16,32 @@ List<Store> storeItems = [
     itemImage: "https://goo.gl/bYJBPJ",
     itemUrl: "https://www.google.com",
   ),
-
   Store.items(
     itemName: "Flutter",
     itemImage: "https://goo.gl/Du8giw",
     itemUrl: "https://flutter.dev/",
   ),
-
   Store.items(
     itemName: "Android",
     itemImage: "https://goo.gl/vCn5r5",
     itemUrl: "https://www.android.com/",
   ),
-
+  Store.items(
+    itemName: "iOS",
+    itemImage:
+        "https://www.clipartmax.com/png/full/190-1905305_ios-logo-icon-os-x-icon-pack-apk.png",
+    itemUrl: "https://www.apple.com//",
+  ),
   Store.items(
     itemName: "YouTube",
     itemImage: "https://goo.gl/cGP6cH",
     itemUrl: "https://www.youtube.com/",
   ),
-
+  Store.items(
+    itemName: "Stack Overflow",
+    itemImage: "https://madsourcer.com/wp-content/uploads/stackoverflow.png",
+    itemUrl: "https://stackoverflow.com/",
+  ),
   Store.items(
     itemName: "GitHub",
     itemImage: "https://goo.gl/u2qZQB",
